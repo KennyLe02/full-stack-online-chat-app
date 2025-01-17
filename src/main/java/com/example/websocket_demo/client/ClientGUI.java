4ddbc4417e6dd0fea91143cdc4a5cdca4dcfe5c0
KennyLe02 <kenny.le02@gmail.com>
2025-01-17 15:04:52 -0500

    Added feature where user can type words, hit enter and it'll appear in the chat message UI

--- src/main/java/com/example/websocket_demo/client/ClientGUI.java
+++ src/main/java/com/example/websocket_demo/client/ClientGUI.java
@@ -1,12 +1,16 @@
 package com.example.websocket_demo.client;
 
+import com.example.websocket_demo.Message;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ClientGUI extends JFrame {
-    private JPanel connectedUsersPanel;
+    private JPanel connectedUsersPanel,messagePanel;
 
     public ClientGUI(String username){
         super("User: " + username);
@@ -29,9 +33,11 @@ public class ClientGUI extends JFrame {
     }
     private void addGuiComponents(){
         addConntectedUsersComponents();
+        addChatComponents();
     }
     private void addConntectedUsersComponents(){
         connectedUsersPanel = new JPanel();
+        connectedUsersPanel.setBorder(Utilities.addPadding(10,10,10,10));
         connectedUsersPanel.setLayout(new BoxLayout(connectedUsersPanel, BoxLayout.Y_AXIS));
         connectedUsersPanel.setBackground(Utilities.secondaryColor);
         connectedUsersPanel.setPreferredSize(new Dimension(200,getHeight()));
@@ -43,5 +49,69 @@ public class ClientGUI extends JFrame {
 
         add(connectedUsersPanel,BorderLayout.WEST);
 
+    }
+
+    private void addChatComponents(){
+        JPanel chatPanel = new JPanel();
+        chatPanel.setLayout(new BorderLayout());
+        chatPanel.setBackground(Utilities.transparentColor);
+
+        messagePanel = new JPanel();
+        messagePanel.setLayout(new BoxLayout(messagePanel,BoxLayout.Y_AXIS));
+        messagePanel.setBackground(Utilities.transparentColor);
+        chatPanel.add(messagePanel, BorderLayout.CENTER);
+
+        messagePanel.add(createChatMessageConponent(new Message("Kenny","Hello world!")));
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBorder(Utilities.addPadding(10,10,10,10));
+        inputPanel.setLayout(new BorderLayout());
+        inputPanel.setBackground(Utilities.transparentColor);
+
+        JTextField inputField = new JTextField();
+        inputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER){
+                    String input = inputField.getText();
+
+                    //edge case: empty message(prevent empty messages)
+                    if (input.isEmpty()) return;
+
+                    inputField.setText("");
+
+                    messagePanel.add(createChatMessageConponent(new Message("Kenny",input)));
+                    repaint();
+                    revalidate();
+                }
+            }
+        });
+        inputField.setBackground(Utilities.secondaryColor);
+        inputField.setForeground(Utilities.textColor);
+        inputField.setFont(new Font("Inter",Font.PLAIN,16));
+        inputField.setPreferredSize(new Dimension(inputPanel.getWidth(),50));
+        inputPanel.add(inputField,BorderLayout.CENTER);
+        chatPanel.add(inputField,BorderLayout.SOUTH);
+
+
+        add(chatPanel,BorderLayout.CENTER);
+    }
+    private JPanel createChatMessageConponent(Message message){
+        JPanel chatMessage = new JPanel();
+        chatMessage.setBackground(Utilities.transparentColor);
+        chatMessage.setLayout(new BoxLayout(chatMessage, BoxLayout.Y_AXIS));
+        chatMessage.setBorder(Utilities.addPadding(20,20,10,20));
+
+        JLabel usernameLabel = new JLabel(message.getUser());
+        usernameLabel.setFont(new Font("Inter", Font.BOLD,18));
+        usernameLabel.setForeground(Utilities.textColor);
+        chatMessage.add(usernameLabel);
+
+        JLabel messageLabel = new JLabel(message.getMessage());
+        messageLabel.setFont(new Font("Inter",Font.PLAIN,18));
+        messageLabel.setForeground(Utilities.textColor);
+        chatMessage.add(messageLabel);
+
+        return chatMessage;
     }
 }
--- src/main/java/com/example/websocket_demo/client/Utilities.java
+++ src/main/java/com/example/websocket_demo/client/Utilities.java
@@ -1,5 +1,6 @@
 package com.example.websocket_demo.client;
 
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class Utilities {
@@ -7,4 +8,8 @@ public class Utilities {
     public static final Color primaryColor = Color.decode("#2F2D2D");
     public static final Color secondaryColor = Color.decode("#484444");
     public static final Color textColor = Color.white;
+
+    public static EmptyBorder addPadding(int top, int left, int bottom, int right){
+        return new EmptyBorder(top, left, bottom,right);
+    }
 }
